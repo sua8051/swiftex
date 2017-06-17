c4d92670086f7c4de3cb9dd3745ff8bc3975383e
Sua Le <sual@dekiconnect.com>
2017-06-17 14:32:00 +0700

    Initial Commit

---
+++ Swiftext/StringExt.swift
@@ -0,0 +1,14 @@
+//
+//  StringExt.swift
+//  Swiftext
+//
+//  Created by Sua Le on 6/17/17.
+//  Copyright © 2017 Sua Le. All rights reserved.
+//
+
+import Foundation
+
+
+extension String {
+        
+}
---
+++ Swiftext/Swiftext.h
@@ -0,0 +1,19 @@
+//
+//  Swiftext.h
+//  Swiftext
+//
+//  Created by Sua Le on 6/17/17.
+//  Copyright © 2017 Sua Le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swiftext.
+FOUNDATION_EXPORT double SwiftextVersionNumber;
+
+//! Project version string for Swiftext.
+FOUNDATION_EXPORT const unsigned char SwiftextVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swiftext/PublicHeader.h>
+
+
