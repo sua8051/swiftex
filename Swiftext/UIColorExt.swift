0e465b8cfef730b99c9df082b09584a9d815c261
Minh Luan Tran <noblakit01@gmail.com>
2017-07-06 01:12:33 -0700

    Add public init UIColor

--- Swiftext/UIColorExt.swift
+++ Swiftext/UIColorExt.swift
@@ -9,11 +9,11 @@
 import UIKit
 
 extension UIColor {
-    convenience init(red: Int, green: Int, blue: Int) {
+    public convenience init(red: Int, green: Int, blue: Int) {
         self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
     }
     
-    convenience init(hex: Int) {
+    public convenience init(hex: Int) {
         self.init(red: (hex >> 16) & 0xff, green: (hex >> 8) & 0xff, blue: hex & 0xff)
     }
 }
